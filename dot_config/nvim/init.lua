7c5a93b230d3d746f40f471c84c75c524571114b
Carlos Pedreira <carlospedreira@users.noreply.github.com>
2026-05-24 22:49:46 +0100

    feat: add neovim with pinned version and basic config
    Install neovim 0.12.2 via mise and provide a sensible default
    init.lua with line numbers, mouse support, clipboard integration,
    and leader-key shortcuts.
    
    Planned by Carlos Pedreira, Implemented by Kimi 2.6, Reviewed by Carlos Pedreira

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,20 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.cmd('syntax on')
+vim.opt.termguicolors = true
+vim.opt.showmatch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.wildmenu = true
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>w', ':w<CR>', { silent = true })
+vim.keymap.set('n', '<leader>q', ':q<CR>', { silent = true })
